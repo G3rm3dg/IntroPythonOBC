8455db79e63071d83713c53d4203908e81b2d3cf
Gerley <117965738+G3rm3dg@users.noreply.github.com>
2023-05-26 20:50:52 -0500

    Ejercicio 4
    Ejercicio 4, muestra los números del 1 al 100 de manera inversa

---
+++ EJERCICIO 4/Ejercio4.py
@@ -0,0 +1,10 @@
+print("\nEJERCICIO 4, OPEN BOOT CAMP \n")
+print("ejercicio para mostrar en pantalla de 100 hasta 0\n")
+
+contador=100
+
+
+while contador >= 1:
+    print("el numero es igual a: ", contador, "\n")
+    contador -= 1
+print("\nFIN DE LA EJECUCION")
